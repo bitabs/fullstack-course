b5d7042626c465c74f30305f291e69911a81da1c
Naseebullah Safi <javastastic@gmail.com>
2019-03-29 00:38:49 +0000

    tab change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 const
-    path    = require('path'),
-    webpack = require('webpack');
+  path = require('path'),
+  webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                options: {
-                    presets: [
-                        "@babel/env"
-                    ]
-                }
-            },
-            {
-                test: /\.css/,
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ]
-            }
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/env"
+          ]
+        }
+      },
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          "css-loader"
         ]
-    },
-    resolve: {
-        extensions: ["*", ".js", ".jsx"]
-    },
-    output: {
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public/"),
-        port: 3000,
-        publicPath: "http://localhost:3000/dist/",
-        hot: true
-    },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hot: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
