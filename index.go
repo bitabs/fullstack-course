7b9d0d8e6ef232135632778ca1cecdbad90bf3a0
naseebullahahmadi <naseebullah.ahmadi@tribalworldwide.co.uk>
2019-06-14 17:34:30 +0100

    testing

--- index.go
+++ index.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-contrib/static"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
 )
 
 func main() {
 	// Set the router as the default one shipped with Gin
-	router := gin.Default()
+	//router := gin.Default()
+
+	http.Handle("/", http.FileServer(http.Dir("./build")))
 
 	// Serve frontend static files
-	router.Use(static.Serve("/", static.LocalFile("./build", true)))
+	//router.Use(static.Serve("/", static.LocalFile("./build", true)))
 
-	// Setup route group for the API
-	api := router.Group("/api")
-	{
-		api.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "pong",
-			})
-		})
-	}
+	//// Setup route group for the API
+	//api := router.Group("/api")
+	//{
+	//	api.GET("/", func(c *gin.Context) {
+	//		c.JSON(http.StatusOK, gin.H{
+	//			"message": "pong",
+	//		})
+	//	})
+	//}
 
 	// Start and run the server
-	router.Run(GetPort())
+	//router.Run(GetPort())
 }
 
 func GetPort() string {
