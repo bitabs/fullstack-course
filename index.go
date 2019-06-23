42043c6d0e19c7319918af99d4f9da5a147637c8
naseebullahahmadi <naseebullah.ahmadi@tribalworldwide.co.uk>
2019-06-23 12:13:34 +0100

    logging

--- index.go
+++ index.go
@@ -7,26 +7,7 @@ import (
 )
 
 func main() {
-	// Set the router as the default one shipped with Gin
-	//router := gin.Default()
-
 	http.Handle("/", http.FileServer(http.Dir("./build")))
-
-	// Serve frontend static files
-	//router.Use(static.Serve("/", static.LocalFile("./build", true)))
-
-	//// Setup route group for the API
-	//api := router.Group("/api")
-	//{
-	//	api.GET("/", func(c *gin.Context) {
-	//		c.JSON(http.StatusOK, gin.H{
-	//			"message": "pong",
-	//		})
-	//	})
-	//}
-
-	// Start and run the server
-	//router.Run(GetPort())
 }
 
 func GetPort() string {
