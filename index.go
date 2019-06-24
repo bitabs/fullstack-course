02434590fa9175ec9d4b99bfebc3e65ea3b5cb24
naseebullahahmadi <naseebullah.ahmadi@tribalworldwide.co.uk>
2019-06-24 15:57:45 +0100

    Debugging Go + React

--- index.go
+++ index.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./build")))
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello Mars!")
+	}))
+	Port := GetPort()
+	log.Println("Now server is running on port" + Port)
+	if err := http.ListenAndServe(Port, nil); err != nil {
+		log.Fatal(err)
+	}
+	//http.Handle("/", http.FileServer(http.Dir("./build")))
 }
 
 func GetPort() string {
 	var port = os.Getenv("PORT")
 	// Set a default port if there is nothing in the environment
 	if port == "" {
-		port = "4000"
+		port = "3000"
 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
 	}
 	return ":" + port
